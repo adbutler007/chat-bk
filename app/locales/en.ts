ad5d632466e506ebebe2240c81f5b044ab199ca7
Adam Butler <adam.butler@investresolve.com>
2023-11-14 20:04:14 +0000

    fixed typo again

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -139,7 +139,7 @@ const en = {
   Welcome: {
     Title: "Return Stacking Chatbot",
     SubTitle:
-      "Get answers to questions about Return Stacking and trend replications strategies.",
+      "Get answers to questions about Return Stacking and trend replication strategies.",
     Quote:
       "“This tool has saved me countless hours of work and helped me apply AI features to my work faster than ever before.”",
     LoginLinkedinTitle: "Login with LinkedIn",
